03179594eb247f4d1270e15c40e56958de830bc7
Nandakumar-28 <142591909+Nandakumar-28@users.noreply.github.com>
2023-12-15 22:24:26 +0530

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -6861,7 +6861,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 const environment = {
     production: false,
-    apiUrl: "https://vewin.vewinpro.com/"
+    apiUrl: " https://6c85-27-63-222-34.ngrok-free.app/"
 };
 
 
